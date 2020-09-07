28064c3af63824398e38f67d60568ca9ca348fb7
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-09-07 14:42:47 -0400

    Test commit 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <p><a href="www.google.com">A test</a></p>
+        <p><a href="https://www.google.com">A test</a></p>
       </header>
     </div>
   );
