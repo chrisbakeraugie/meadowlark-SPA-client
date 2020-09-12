f88171dc0a351ef5352018070898692164581a43
chrisbakeraugie <42625149+chrisbakeraugie@users.noreply.github.com>
2020-09-12 12:26:37 -0400

    Update Michael.js

--- src/Michael.js
+++ src/Michael.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function Michael() {
   return (
     <div>
+    <h1>Michael is chris less attractive borther</h1>
       <h2>Michael IS SOOOOOO GREAT</h2>
       <h3><i>He totally does.</i></h3>
     </div>
